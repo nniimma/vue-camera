8d0f7923b5788c4456fb0b52ce1840a9dbe6d8ef
Nima <alireza.khazforoosh@adventistas.org>
2024-05-11 19:53:09 -0300

    camera project done

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import camera from "./components/camera.vue";
+</script>
+
+<template>
+  <div class="min-h-screen flex items-center justify-center">
+    <camera/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
